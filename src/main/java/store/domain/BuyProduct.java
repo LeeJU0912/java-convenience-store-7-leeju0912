93a2d29a87b2a74cc12b33a51b0c1de6a455a0ad
LeeJU <thirdspirit@naver.com>
2024-11-11 02:05:17 +0900

    feat(BuyProduct): 사용자 구매 물건 기록 domain 구현
    최대한 객체 안에 계산 로직을 넣으려고 하였음.

---
+++ src/main/java/store/domain/BuyProduct.java
@@ -0,0 +1,37 @@
+package store.domain;
+
+public class BuyProduct {
+    private String name;
+    private Long quantity;
+    private Long price;
+
+    public BuyProduct(String name, Long quantity, Long price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public Long calculatePriceSum() {
+        return quantity * price;
+    }
+
+    public void reduceQuantity(Long quantity) {
+        this.quantity -= quantity;
+    }
+
+    public void addQuantity(Long quantity) {
+        this.quantity += quantity;
+    }
+}
