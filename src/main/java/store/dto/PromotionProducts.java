121a89177676314f9650379a4c780b175e8e7666
LeeJU <thirdspirit@naver.com>
2024-11-11 02:12:37 +0900

    feat(PromotionProducts): Promotion이 존재하는 객체만 따로 모은 dto 구현
    Products 객체와 다른 점은 Promotion이 반드시 존재하는 객체이기 때문에 extends로 기존 Products 객체 속성을 상속 받아 구현함

---
+++ src/main/java/store/dto/PromotionProducts.java
@@ -0,0 +1,46 @@
+package store.dto;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import store.domain.BuyProduct;
+import store.domain.Product;
+
+import java.time.LocalDateTime;
+
+import static java.lang.Math.min;
+
+public class PromotionProducts extends Products {
+
+    public boolean nowInPromotionDate(String productName) {
+        if (getProductByName(productName) == null) {
+            return false;
+        }
+
+        LocalDateTime now = DateTimes.now();
+        Product promotionProduct = getProducts().get(productName);
+        if (promotionProduct.getPromotion().promoteStartDate().isBefore(now)
+                && promotionProduct.getPromotion().promoteEndDate().isAfter(now)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Long getPromotionValidQuantity(String productName) {
+        return getProducts().get(productName).getPromotion().promoteQuantity();
+    }
+
+    public Long getPromotionProductStockQuantity(String productName) {
+        return getProducts().get(productName).getStock();
+    }
+
+    public Long getPromotionOnlyProductQuantity(BuyProduct buyProduct) {
+        Product product = getProducts().get(buyProduct.getName());
+        return min(product.getStock(), buyProduct.getQuantity()) / (product.getPromotion().promotePlus() + product.getPromotion().promoteQuantity());
+    }
+
+    public void addPromotionQuantity(String productName, Long quantity) {
+        getProducts().get(productName).updateStock(quantity);
+    }
+    public void reducePromotionQuantity(String productName, Long quantity) {
+        getProducts().get(productName).reduceStock(quantity);
+    }
+}
