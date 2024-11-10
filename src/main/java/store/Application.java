974d34293167b5cc330f4cbe2f5a6e8e5573143e
LeeJU <thirdspirit@naver.com>
2024-11-11 02:03:38 +0900

    feat(Application): 팩토리 패턴을 이용한 메인 Controller 생성
    Controller 생성 후 run() 으로 실행

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,12 @@
 package store;
 
+import store.controller.StoreController;
+import store.controller.StoreControllerFactory;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StoreController storeController = StoreControllerFactory.getStoreController();
+        storeController.run();
     }
 }
