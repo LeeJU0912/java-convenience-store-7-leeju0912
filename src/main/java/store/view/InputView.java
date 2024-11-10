85e224f27985071a14fc96065a8708544a511908
LeeJU <thirdspirit@naver.com>
2024-11-11 02:06:42 +0900

    feat(InputView): 사용자 입력 View 객체 구현

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,14 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputBuyProducts() {
+        return Console.readLine();
+    }
+
+    public static String inputYesOrNo() {
+        return Console.readLine();
+    }
+}
