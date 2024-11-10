3f49aec14dd19d9e77412a563ad50cc49f4caa4b
LeeJU <thirdspirit@naver.com>
2024-11-11 02:08:46 +0900

    feat(product): products.md 파일 정보 저장 객체 구현
    products.md 파일 한 줄 정보 당 하나의 객체를 배정

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,54 @@
+package store.domain;
+
+public class Product {
+
+    private String name;
+    private Long price;
+    private Long stock;
+    private String promotionName;
+
+    private Promotion promotion;
+
+    public Product(String name, Long price, Long stock, String promotionName) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.promotionName = promotionName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public String getPromotionName() {
+        return promotionName;
+    }
+
+    public Promotion getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(Promotion promotion) {
+        this.promotion = promotion;
+    }
+
+    public Long getPriceSum() {
+        return price * stock;
+    }
+
+    public void updateStock(Long stock) {
+        this.stock += stock;
+    }
+
+    public void reduceStock(Long stock) {
+        this.stock -= stock;
+    }
+}
