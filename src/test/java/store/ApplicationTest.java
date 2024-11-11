9c2b66fde5c9e1d4ed1f26a2b1bf7534ba1eeffe
LeeJU <thirdspirit@naver.com>
2024-11-11 13:49:59 +0900

    test(ApplicationTest): 테스트 케이스 추가

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -70,6 +70,48 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 프로모션_미적용_알림_구매() {
+        assertSimpleTest(() -> {
+            run("[콜라-3],[에너지바-5]", "Y", "Y", "[콜라-10]", "Y", "N", "N");
+            assertThat(output()).contains("현재 콜라 4개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈8,100", "내실돈8,000");
+        });
+    }
+
+    @Test
+    void 프로모션_끼워주기_알림_구매() {
+        assertSimpleTest(() -> {
+            run("[오렌지주스-1]", "Y", "N", "N");
+            assertThat(output()).contains("현재 오렌지주스은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈1,800");
+        });
+    }
+
+    @Test
+    void 여러번_프로모션_상품_구매2() {
+        assertSimpleTest(() -> {
+            run("[콜라-3]", "N", "Y", "[콜라-4]", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈2,000", "내실돈1,800");
+        });
+    }
+
+    @Test
+    void 여러번_프로모션_상품_구매3() {
+        assertSimpleTest(() -> {
+            run("[콜라-1]", "N", "Y", "[콜라-1]", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈1,000", "내실돈700");
+        });
+    }
+
+    @Test
+    void 여러번_프로모션_상품_구매4() {
+        assertSimpleTest(() -> {
+            run("[콜라-2]", "Y", "N", "Y", "[콜라-2]", "N", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈2,000", "내실돈1,400");
+        });
+    }
+
+    @Test
     void 기간에_해당하지_않는_프로모션_적용() {
         assertNowTest(() -> {
             run("[감자칩-2]", "N", "N");
@@ -82,6 +124,22 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             runException("[컵라면-12]", "N", "N");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 예외_테스트_파싱_오류() {
+        assertSimpleTest(() -> {
+            runException("[컵라면12]", "N", "N");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 예외_테스트_파싱_오류2() {
+        assertSimpleTest(() -> {
+            runException("컵라면-12", "N", "N");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         });
     }
 
